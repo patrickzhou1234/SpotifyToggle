986a169c5e090c441334b89849e224bb4cbf6859
patrickzhou1234 <patrickchang44543@gmail.com>
2023-05-13 11:46:33 -0700

    fixed gettoken

--- pause.py
+++ pause.py
@@ -1,7 +1,7 @@
 import requests, json, time
 from getToken import getToken
 
-if (time.time()>float(open("variables.txt", "r").read().split("\n")[0])):
+if (int(time.time())*1000>int(open("variables.txt", "r").read().split("\n")[0])):
     accessToken = getToken()
 else:
     accessToken = str(open("variables.txt", "r").read().split("\n")[1])
